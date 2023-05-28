1710a47e359b4ed12ac066bba9432f2b4bcf88d5
fetti <fetti@DESKTOP-VD54E8B>
2023-05-28 19:19:30 +0300

    Add project files.

---
+++ Ex2Lab2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Ex2Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnCopiere = new System.Windows.Forms.Button();
+            this.btnStergere = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(33, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(208, 404);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(533, 35);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(208, 404);
+            this.listBox2.TabIndex = 1;
+            // 
+            // btnCopiere
+            // 
+            this.btnCopiere.Location = new System.Drawing.Point(309, 100);
+            this.btnCopiere.Name = "btnCopiere";
+            this.btnCopiere.Size = new System.Drawing.Size(156, 68);
+            this.btnCopiere.TabIndex = 2;
+            this.btnCopiere.Text = "Copiaza";
+            this.btnCopiere.UseVisualStyleBackColor = true;
+            this.btnCopiere.Click += new System.EventHandler(this.btnCopiere_Click);
+            // 
+            // btnStergere
+            // 
+            this.btnStergere.Location = new System.Drawing.Point(309, 260);
+            this.btnStergere.Name = "btnStergere";
+            this.btnStergere.Size = new System.Drawing.Size(156, 68);
+            this.btnStergere.TabIndex = 3;
+            this.btnStergere.Text = "Sterge";
+            this.btnStergere.UseVisualStyleBackColor = true;
+            this.btnStergere.Click += new System.EventHandler(this.btnStergere_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(585, 648);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(156, 68);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 728);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStergere);
+            this.Controls.Add(this.btnCopiere);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnCopiere;
+        private System.Windows.Forms.Button btnStergere;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Ex2Lab2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex2Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines("info.txt");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                listBox1.Items.Add(lines[i]);
+            }
+        }
+
+        private void btnCopiere_Click(object sender, EventArgs e)
+        {
+            foreach (var item in listBox1.SelectedItems)
+            {
+                listBox2.Items.Add(item);
+            }
+        }
+
+        private void btnStergere_Click(object sender, EventArgs e)
+        {
+            if(listBox2.SelectedItem != null)
+            {
+                listBox2.Items.Remove(listBox2.SelectedItem);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
